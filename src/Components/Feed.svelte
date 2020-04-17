273fe75f7192888f2186cee28003e63b7123d07a
nkry <nickr2093@gmail.com>
2020-04-17 13:49:22 +0200

    ...

--- src/Components/Feed.svelte
+++ src/Components/Feed.svelte
@@ -1,8 +1,18 @@
 <script>
+  import moment from 'moment'
   // export let url
+  // 15-03-2020
+  let start = moment("2020-03-15", "YYYY-MM-DD");
+  let end = moment()
+  let endFormat = end.format('LL')
+  let daysPassed = parseInt(moment.duration(end.diff(start)).asDays())
 </script>
 
 <div class = {"feed-container"}>
+  <div class = "feed-container_inner">
+    Today is {endFormat}<br/>
+    We have passed {daysPassed} days alone together
+  </div>
 </div>
 
 <style lang="scss" global>
@@ -16,7 +26,14 @@
     border-left: 0;
     border-bottom: 0;
     border-right: 0;
-    color: white;
+    color: $lime;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    &_inner {
+      @include type-sans-lg;
+      text-align: center;
+    }
   }
 
 </style>
