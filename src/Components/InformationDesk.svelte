d3e4042b462fae92ca5f0a76607674dd648ff2a4
nkry <nickr2093@gmail.com>
2020-04-24 20:50:13 +0200

    credits

--- src/Components/InformationDesk.svelte
+++ src/Components/InformationDesk.svelte
@@ -7,6 +7,7 @@
     <p><a href="http://pub.sandberg.nl/" target="_blank">PUB Sandberg</a> initiated an online platform that serves as a channel for the Sandberg community to share and communicate during times of isolation.</p>
     <p>While separated, we want to feel each other, beyond the screen or melting its surface to bring us closer, just like a bright, silvery mist. Chatting in the cantine, reading sessions in the library, radio streamings for withdrawal, and monologues for learning about togetherness will happen.</p>
     <p>For more info, email us at <a href = "mailto:pub.sandberg@gmail.com">pub.sandberg@gmail.com</a></p>
+    <p>Website by <a href="https://www.zelda.fi/" target="_blank">Hanna</a>, <a href="http://leithbenkhedda.com/" target="_blank">Leith</a>, <a href="http://an-area.site/" target="_blank">Nick</a> and <a href="https://saschakrischock.com/" target="_blank">Sascha</a>.</p>
   </div>
 </div>
 
@@ -19,11 +20,14 @@
     // height: 100%;
     display: flex;
     padding: .5rem;
-    @include type-serif-lg;
     color: white;
     padding-bottom: calc(#{$footerHeight} * 2);
     & p {
+      @include type-serif-lg;
       margin-bottom: 1rem;
+      &:last-child {
+        @include type-serif-md;
+      }
       & a {
         text-decoration: none;
         color: $lime;
